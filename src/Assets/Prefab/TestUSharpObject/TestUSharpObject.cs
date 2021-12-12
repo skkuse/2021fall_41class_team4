6646d370fd500365552bc6a9bc13abab0d8d5003
pgj123 <dbfltkfkd01@gmail.com>
2021-12-12 23:07:31 +0900

    commit

---
+++ src/Assets/Prefab/TestUSharpObject/TestUSharpObject.cs
@@ -0,0 +1,18 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class TestUSharpObject : UdonSharpBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, 90f * Time.deltaTime);
+    }
+}
